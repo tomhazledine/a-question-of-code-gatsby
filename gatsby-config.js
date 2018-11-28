a9fd5b37adf56272baff7322f51d9413363a20ec
Tom Hazledine <tom.hazledine@creation.agency>
2018-11-28 22:14:12 +0000

    getting file data with source-filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
     siteMetadata: {
         title: `A question of code`
     },
-    plugins: [`gatsby-plugin-emotion`]
+    plugins: [
+        `gatsby-plugin-emotion`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`
+            }
+        }
+    ]
 };
