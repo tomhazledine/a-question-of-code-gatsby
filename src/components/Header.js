dbc411e37d01f541cd8a9f0307dc628830320bcf
Tom Hazledine <tom.hazledine@creation.agency>
2018-11-28 19:46:01 +0000

    adding emotion for css-in-js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { Link } from "gatsby";
+import { css } from "emotion";
 
 const Header = () => (
-    <div>
+    <div
+        className={css`
+            display: flex;
+            justify-content: space-between;
+            border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+            margin-bottom: 1em;
+        `}
+    >
         <h1>A Question of Code</h1>
-        <ul>
+        <ul
+            className={css`
+                display: flex;
+                justify-content: space-between;
+                li {
+                    margin: 0 0.5em;
+                    list-style: none;
+                }
+            `}
+        >
             <li>
                 <Link to="/">Home</Link>
             </li>
